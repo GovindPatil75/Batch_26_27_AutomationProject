9b6bbb7c5b9a6f707e0fd4475f4fd899bffe98d6
GovindPatil75 <mulegr75@gmail.com>
2024-08-29 09:21:45 +0530

    Absolute Xpath Test

---
+++ src/test/java/com/TestSample/AbsoluteXpathTest.java
@@ -0,0 +1,34 @@
+package com.TestSample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AbsoluteXpathTest {
+
+	public static void main(String[] args) {
+		
+		//Browser Open 
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		// WebElement Identify -- Absolute Xpath Expression
+		
+	WebElement Email=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));	
+	Email.sendKeys("Test");
+	
+	WebElement password=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input"));
+	password.sendKeys("12345678");
+	
+	WebElement LoginBtn=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button"));
+	LoginBtn.click();
+	
+	driver.close();
+	
+	}
+
+}
