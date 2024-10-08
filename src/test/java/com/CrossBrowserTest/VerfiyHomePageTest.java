c28ec376319e362c6c254a62bc8b2a84f7ea82e9
GovindPatil75 <mulegr75@gmail.com>
2024-10-08 09:15:30 +0530

    Cross browser Test

---
+++ src/test/java/com/CrossBrowserTest/VerfiyHomePageTest.java
@@ -0,0 +1,20 @@
+package com.CrossBrowserTest;
+
+import org.testng.annotations.Test;
+
+public class VerfiyHomePageTest extends BaseClass{
+
+	@Test
+	public void TC_002() {
+		
+		System.out.println("Test TC 002");
+	}
+	
+	@Test
+	public void TC_003() {
+		
+		System.out.println("Test TC 003");
+	}
+	
+	
+}
---
+++ src/test/java/com/CrossBrowserTest/VerifyLoginTest.java
@@ -0,0 +1,13 @@
+package com.CrossBrowserTest;
+
+import org.testng.annotations.Test;
+
+public class VerifyLoginTest extends BaseClass{
+
+	
+	@Test
+	public void TC_001() {
+		
+		System.out.println("Test TC 001");
+	}
+}
